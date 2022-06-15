e4c2522bc95b69481d85918c4496ec1f6a3c0860
GMartinECU <gabrielmartin@byui.edu>
2022-06-15 09:04:09 -0600

    button

--- Login.js
+++ Login.js
@@ -1,10 +1,13 @@
 import {useState} from "react";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
-import { black } from "react-native-paper/lib/typescript/styles/colors";
+import { Text, TouchableOpacity, View } from "react-native";
+
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
 
   return (
     <SafeAreaView>
@@ -13,6 +16,7 @@ const Login = () => {
         onChangeText={setPhoneNumber}
         value={phoneNumber}
         placeholder = "###-###-####"
+        placeholderTextColor="#000000"
         keyboardType="default"
 
       />
@@ -21,9 +25,21 @@ const Login = () => {
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
         placeholder="####"
+        placeholderTextColor="#000000"
         keyboardType="numeric"
         secureTextEntry={true}
       />
+
+      
+      {/* button begins here */}
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={onPress}>
+
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -32,9 +48,55 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
-    borderWidth: 1,
-    padding: 10,
+    borderWidth: 2,
+    padding: 1,
+    backgroundColor: "#FFFFFF",
+    
   },
+  margin:{
+    marginTop: 100,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+
+  // buttons begins here
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    height: 100,
+    width: 100,
+    justifyContent: "center"
+    
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  }
 });
+
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.countContainer}>
+        <Text>Count: {count}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 
 export default Login;
