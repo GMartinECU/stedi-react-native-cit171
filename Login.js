4b00a7f92220902cf2e8f3f9ec4ed390b68c730a
GMartinECU <gabrielmartin@byui.edu>
2022-06-18 16:42:02 -0600

    app still runs

--- Login.js
+++ Login.js
@@ -3,6 +3,18 @@ import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 import { Text, TouchableOpacity, View } from "react-native";
 
 
+const sendText = async (phoneNumber) => {
+  
+  // using fetch do a POST to https://dev.stedi.me/twofactorlogin/8056688113
+  await fetch('https://dev.stedi.me/twofactorlogin/'+phoneNumber),{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/text'
+    }
+  }
+  console.log("Phone Number:", phoneNumber);
+}
+
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
@@ -15,17 +27,17 @@ const Login = () => {
         style={styles.input}
         onChangeText={setPhoneNumber}
         value={phoneNumber}
-        placeholder = "###-###-####"
-        placeholderTextColor="#000000"
-        keyboardType="default"
+        placeholder = "Enter your phone number"
+        placeholderTextColor="#8e8e8e"
+        keyboardType="numeric"
 
       />
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
-        placeholder="####"
-        placeholderTextColor="#000000"
+        placeholder="Enter the code we sent you"
+        placeholderTextColor="#8e8e8e"
         keyboardType="numeric"
         secureTextEntry={true}
       />
@@ -35,9 +47,17 @@ const Login = () => {
       <View>
         <TouchableOpacity
           style={styles.button}
-          onPress={onPress}>
-
-          <Text>Login</Text>
+          onPress={()=>{sendText(phoneNumber)}}>
+              
+          <Text style = {styles.text}>Send Text</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={()=>{sendText(phoneNumber)}}>
+              
+          <Text style = {styles.text}>Login</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -65,38 +85,42 @@ const styles = StyleSheet.create({
   // buttons begins here
   button: {
     alignItems: "center",
-    backgroundColor: "#DDDDDD",
+    backgroundColor: "#2b80b6",
     padding: 10,
-    height: 100,
-    width: 100,
-    justifyContent: "center"
-    
+    height: 75,
+    margin: 10,
+    justifyContent: "center",
   },
+
+  text: {
+    color: "#FFFFFF",
+  },
+
   countContainer: {
     alignItems: "center",
     padding: 10
   }
 });
 
+// No idea what this is but im going to leave it here
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const onPress = () => setCount(prevCount => prevCount + 1);
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(prevCount => prevCount + 1);
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.countContainer}>
-        <Text>Count: {count}</Text>
-      </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={onPress}
-      >
-        <Text>Press Here</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.countContainer}>
+//         <Text>Count: {count}</Text>
+//       </View>
+//       <TouchableOpacity
+//         style={styles.button}
+//         onPress={onPress}
+//       >
+//         <Text>Press Here</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
 
 
 export default Login;
